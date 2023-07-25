a9cf0315ccad0b0d5bf213b1794ef1704ef6b0d8
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-07-25 12:18:15 -0700

    added bonus features, added add'l sql queries to create list choices for user prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,9 +12,9 @@ VALUES
 ('Comms Specialist', 40000.00, 3),
 ('Tech Analyst', 60000.00, 4);
 
-INSERT INTO employee (first_name,last_name,role_id)
+INSERT INTO employee (first_name,last_name,role_id, manager_id)
 VALUES
-('John', 'Smith', 1),
-('Lucy', 'Brown', 2),
-('Sara', 'Johnson', 3),
-('Tad', 'Gonzales', 4);
+('Lucy', 'Brown', 1, null),
+('John', 'Smith', 2, 1),
+('Sara', 'Johnson', 3, 2),
+('Tad', 'Gonzales', 4, 1);
